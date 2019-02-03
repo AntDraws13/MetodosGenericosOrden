0ff496646f42c36098ed606035272a84cc579183
Isaí Hinojos <44847084+AntDraws13@users.noreply.github.com>
2019-02-03 14:22:15 -0700

    Add files via upload
    Finale

--- MétodosOrdenGenérico/src/app/Main.java
+++ MétodosOrdenGenérico/src/app/Main.java
@@ -4,6 +4,11 @@ import java.util.Random;
 import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
+/**
+ * 
+ * @author ant
+ */
+
 public class Main {
 
     public static Integer[] aInt;
